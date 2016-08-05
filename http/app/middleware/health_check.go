ff5232a4e08aab10dfe2574fae3a58af710f20d4
Guoqiang Chen <subchen@gmail.com>
2016-08-05 12:35:53 +0800

    fixes health_check middleware

--- http/app/middleware/health_check.go
+++ http/app/middleware/health_check.go
@@ -7,21 +7,20 @@ import (
 )
 
 func HealthCheckHandler(ctx *app.Context) {
-	header := ctx.Header()
-	accept := header.Get("Accept")
+	accept := ctx.Header().Get("Accept")
 
 	if strings.Contains(accept, "application/json") {
-		header.Set("Content-Type", "application/json; charset=utf-8")
+		ctx.ResponseWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
 		fmt.Fprintf(ctx.ResponseWriter, "{\"health\": true}\n")
 		return
 	}
 
 	if strings.Contains(accept, "application/xml") {
-		header.Set("Content-Type", "application/xml; charset=utf-8")
+		ctx.ResponseWriter.Header().Set("Content-Type", "application/xml; charset=utf-8")
 		fmt.Fprintf(ctx.ResponseWriter, "<health>true</health>\n")
 		return
 	}
 
-	header.Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(ctx.ResponseWriter, "OK\n")
+	ctx.ResponseWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(ctx.ResponseWriter, "health OK\n")
 }
